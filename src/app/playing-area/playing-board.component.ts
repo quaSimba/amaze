e91c5741340b3aab929b2331ed151bb843cc4e33
Simon Barthmuß <simon.b@posteo.net>
2017-12-09 12:28:27 +0100

    Adds initial playing board files Adds the initial playing board and pad files. Alters app component files.

---
+++ src/app/playing-area/playing-board.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'playing-board',
+  templateUrl: './playing-board.component.html',
+  styleUrls: ['./playing-board.component.css']
+})
+export class PlayingBoardComponent {
+}
