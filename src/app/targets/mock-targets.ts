5132ef0ec7b854c10157266cb59cd2dfb45bf6b5
Kevin Militzer <Kevin.Militzer@live.de>
2018-01-14 18:38:19 +0100

    shuffle, playerTargets created the targets for the player/s which are shuffled first by the shuffle-service. Also buttons which split the PlayerTargets into parts of equal length per Player.

---
+++ src/app/targets/mock-targets.ts
@@ -0,0 +1,28 @@
+import { targets } from './targets';
+
+export const TARGETS: targets[] = [
+  {id:  0, name:"Bat", path:"../assets/img/Bat.png"},
+  {id:  1, name:"Beetle", path:"../assets/img/Beetle.png"},
+  {id:  2, name:"Book", path:"../assets/img/Book.png"},
+  {id:  3, name:"Candlestick", path:"../assets/img/Candlestick.png"},
+  {id:  4, name:"Chest", path:"../assets/img/Chest.png"},
+  {id:  5, name:"Crown", path:"../assets/img/Crown.png"},
+  {id:  6, name:"Djinn", path:"../assets/img/Djinn.png"},
+  {id:  7, name:"Dragon", path:"../assets/img/Dragon.png"},
+  {id:  8, name:"Emerald", path:"../assets/img/Emerald.png"},
+  {id:  9, name:"Fairy", path:"../assets/img/Fairy.png"},
+  {id:  10, name:"Ghost", path:"../assets/img/Ghost.png"},
+  {id:  11, name:"Gold", path:"../assets/img/Gold.png"},
+  {id:  12, name:"Helmet", path:"../assets/img/Helmet.png"},
+  {id:  13, name:"Keys", path:"../assets/img/Keys.png"},
+  {id:  14, name:"Kobold", path:"../assets/img/Kobold.png"},
+  {id:  15, name:"Lizard", path:"../assets/img/Lizard.png"},
+  {id:  16, name:"Map", path:"../assets/img/Map.png"},
+  {id:  17, name:"Moth", path:"../assets/img/Moth.png"},
+  {id:  18, name:"Mouse", path:"../assets/img/Mouse.png"},
+  {id:  19, name:"Owl", path:"../assets/img/Owl.png"},
+  {id:  20, name:"Ring", path:"../assets/img/Ring.png"},
+  {id:  21, name:"Skull", path:"../assets/img/Skull.png"},
+  {id:  22, name:"Spider", path:"../assets/img/Spider.png"},
+  {id:  23, name:"Sword", path:"../assets/img/Sword.png"}
+];
